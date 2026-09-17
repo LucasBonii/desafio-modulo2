c302792fd0037787e489c6e1b7d6aefc30807d20
LucasBonii <158228644+LucasBonii@users.noreply.github.com>
2026-09-17 15:40:51 -0300

    feat(meals): add update and delete meal routes

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -6,12 +6,9 @@ import { checkSessionIdExists } from '../middlewares/check-session-id-exists.js'
 
 export async function mealsRoutes(app: FastifyInstance) {
 
-  app.post(
-    '/',
-    {
-      preHandler: [checkSessionIdExists],
-    },
-    async (request, reply) => {
+    app.addHook('preHandler', checkSessionIdExists)
+
+    app.post('/', async (request, reply) => {
       const createMealBodySchema = z.object({
         name: z.string().min(1, 'Nome é obrigatório'),
         description: z.string().min(1, 'Descrição é obrigatória'),
@@ -43,6 +40,143 @@ export async function mealsRoutes(app: FastifyInstance) {
       })
 
       return reply.status(201).send()
-    },
-  )
+    })
+
+    app.get('/', async (request, reply) => {
+    const sessionId = request.cookies.sessionId
+
+    const user = await db('users')
+      .where('session_id', sessionId)
+      .first()
+
+    if (!user) {
+      return reply.status(401).send({ error: 'Unauthorized' })
+    }
+
+    const meals = await db('meals')
+      .where('user_id', user.id)
+      .orderBy('date', 'desc')
+
+    return reply.send({ meals })
+  })
+
+
+  app.get('/:id', async (request, reply) => {
+    const getMealParamsSchema = z.object({
+      id: z.string().uuid('ID inválido'),
+    })
+
+    const { id } = getMealParamsSchema.parse(request.params)
+    const sessionId = request.cookies.sessionId
+
+    const user = await db('users')
+      .where('session_id', sessionId)
+      .first()
+
+    if (!user) {
+      return reply.status(401).send({ error: 'Unauthorized' })
+    }
+
+    const meal = await db('meals')
+      .where({
+        id,
+        user_id: user.id,
+      })
+      .first()
+
+    if (!meal) {
+      return reply.status(404).send({ error: 'Meal not found' })
+    }
+
+    return reply.send({ meal })
+  })
+
+  app.put('/:id', async (request, reply) => {
+    const updateMealParamsSchema = z.object({
+      id: z.uuid('ID inválido'),
+    })
+
+    const updateMealBodySchema = z.object({
+      name: z.string().min(1, 'Nome é obrigatório'),
+      description: z.string().min(1, 'Descrição é obrigatória'),
+      isOnDiet: z.boolean(),
+      date: z.coerce.date(),
+    })
+
+    const { id } = updateMealParamsSchema.parse(request.params)
+    const { name, description, isOnDiet, date } = updateMealBodySchema.parse(request.body)
+
+    const sessionId = request.cookies.sessionId
+
+    const user = await db('users')
+      .where('session_id', sessionId)
+      .first()
+
+    if (!user) {
+      return reply.status(401).send({ error: 'Unauthorized' })
+    }
+
+    const meal = await db('meals')
+      .where({
+        id,
+        user_id: user.id,
+      }).first()
+
+    if (!meal) {
+      return reply.status(404).send({ error: 'Meal not found' })
+    }
+
+    await db('meals')
+      .where({
+        id,
+        user_id: user.id,
+      })
+      .update({
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date: date.toISOString(),
+        updated_at: new Date().toISOString(),
+      })
+
+    return reply.status(204).send()
+  })
+
+
+  app.delete('/:id', async (request, reply) => {
+    const deleteMealParamsSchema = z.object({
+      id: z.uuid('ID inválido'),
+    })
+
+    const { id } = deleteMealParamsSchema.parse(request.params)
+    const sessionId = request.cookies.sessionId
+
+    const user = await db('users')
+      .where('session_id', sessionId)
+      .first()
+
+    if (!user) {
+      return reply.status(401).send({ error: 'Unauthorized' })
+    }
+
+    const meal = await db('meals')
+      .where({
+        id,
+        user_id: user.id,
+      }).first()
+
+    if (!meal) {
+      return reply.status(404).send({ error: 'Meal not found' })
+    }
+
+    await db('meals')
+      .where({
+        id,
+        user_id: user.id,
+      })
+      .delete()
+
+    return reply.status(204).send()
+  })
+
 }
